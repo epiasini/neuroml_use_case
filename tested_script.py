a5a70daffc5f342a4f65844c909cd95a2a99c343
Padraig Gleeson <p.gleeson@gmail.com>
2015-02-16 18:13:39 +0000

    Added methods for including spikegenerator populations
    Changed ln() -> log() in spikeGenerators.xml
    Currently only works with jNeuroML, not NEURON...

--- tested_script.py
+++ tested_script.py
@@ -29,6 +29,7 @@ import numpy as np
 import neuroml as nml
 from pyneuroml import pynml
 from pyneuroml.lems.LEMSSimulation import LEMSSimulation
+import lems.api as lems
 
 # set parameters controlling the number of "on" and "off" synaptic
 # inputs. We do this to show why it is convenient to generate the
@@ -37,7 +38,8 @@ n_inputs_ON = 2
 n_inputs_OFF = 2
 
 # load NeuroML components, LEMS components and LEMS componentTypes from external files
-####spike_generator_doc = nml.import_lems("lemsDefinitions/spikeGenerators.xml")
+spike_generator_file_name = "lemsDefinitions/spikeGenerators.xml"
+spike_generator_doc = pynml.read_lems_file(spike_generator_file_name)
 ####spike_recorder_doc = nml.import_lems("lemsDefinitions/spikeRecorder.xml")
 iaf_nml2_file_name = "lemsDefinitions/IaF_GrC.nml"
 IaF_GrC_doc = pynml.read_neuroml2_file(iaf_nml2_file_name)
@@ -49,12 +51,23 @@ IaF_GrC_doc = pynml.read_neuroml2_file(iaf_nml2_file_name)
 # define some components from the componentTypes we just loaded
 ####spike_recorder = spike_recorder_doc.spikeRecorder(id="spikeRecorder")
 
-####generator_ON = spike_generator_doc.spikeGeneratorRefPoisson(id="mossySpikerON",
-########                                                            minimumISI="2 ms",
-####                                                           averageRate="80 Hz")
-####generator_OFF = spike_generator_doc.spikeGeneratorRefPoisson(id="mossySpikerOFF",
-####                                                             minimumISI="2 ms",
-####                                                             averageRate="80 Hz")
+spike_generator_ref_poisson_type = spike_generator_doc.component_types['spikeGeneratorRefPoisson']
+lems_instances_doc = lems.Model()
+
+spike_generator_on = lems.Component("mossySpikerON", 
+                                                      spike_generator_ref_poisson_type.name)
+                                                      
+spike_generator_on.set_parameter("minimumISI", "2 ms")
+spike_generator_on.set_parameter("averageRate", "80 Hz")
+lems_instances_doc.add(spike_generator_on)
+
+spike_generator_off = lems.Component("mossySpikerOFF", 
+                                                      spike_generator_ref_poisson_type.name)
+                                                      
+spike_generator_off.set_parameter("minimumISI", "2 ms")
+spike_generator_off.set_parameter("averageRate", "80 Hz")
+lems_instances_doc.add(spike_generator_off)
+
 
 # rename some components for convenience
 IaF_GrC = IaF_GrC_doc.iaf_ref_cells[0] # note that here IaF_GrC_doc.IaF_GrC is
@@ -73,21 +86,22 @@ GrCPop = nml.Population(id="GrCPop",
 '''
 SpikeRecorderPop = nml.Population(id="SpikeRecorderPop",
                                   component="IaF_GrC",
-                                  size=1)
-mossySpikersPopON = nml.Population(id="mossySpikersPopON",
-                                   component="mossySpikerON",
+                                  size=1)'''
+                                  
+mossySpikersPopON = nml.Population(id=spike_generator_on.id+"Pop",
+                                   component=spike_generator_on.id,
                                    size=n_inputs_ON)
-mossySpikersPopOFF = nml.Population(id="mossySpikersPopOFF",
-                                    component="mossySpikerOFF",
-                                    size=n_inputs_OFF)'''
+mossySpikersPopOFF = nml.Population(id=spike_generator_off.id+"Pop",
+                                    component=spike_generator_off.id,
+                                    size=n_inputs_OFF)
 # create network and add populations
 net = nml.Network(id="network")
 net_doc = nml.NeuroMLDocument(id=net.id)
 net_doc.networks.append(net)
 net.populations.append(GrCPop)
 ####net.populations.append(SpikeRecordersPop)
-####net.populations.append(mossySpikersPopON)
-####net.populations.append(mossySpikersPopOFF)
+net.populations.append(mossySpikersPopON)
+net.populations.append(mossySpikersPopOFF)
 '''
 # set up connections
 for stim_pop in [mossySpikersPopON, mossySpikersPopOFF]:
@@ -109,6 +123,11 @@ net.append(nml.SynapticConnection(from_="GrCPop[0]",
 net_file_name = 'generated_network.net.nml'
 pynml.write_neuroml2_file(net_doc, net_file_name)
 
+# Write LEMS instances to file
+lems_instances_file_name = 'instances.xml'
+pynml.write_lems_file(lems_instances_doc, lems_instances_file_name)
+
+
 
 # Create a LEMSSimulation to manage creation of LEMS file
 duration = 1000  # ms
@@ -122,12 +141,17 @@ ls.assign_simulation_target(net.id)
 
 # Include generated/existing NeuroML2 files
 ls.include_neuroml2_file(iaf_nml2_file_name)
+ls.include_lems_file(spike_generator_file_name, include_included=False)
+ls.include_lems_file(lems_instances_file_name)
 ls.include_neuroml2_file(net_file_name)
 
 
 # Specify Displays and Output Files
-disp0 = "display1"
+disp0 = "display_voltages"
 ls.create_display(disp0, "Voltages", "-95", "-38")
+
+disp1 = "display_spike_generators"
+ls.create_display(disp1, "Spike generators", "-10", "80")
 
 of0 = 'Volts_file'
 ls.create_output_file(of0, "v.dat")
@@ -136,13 +160,21 @@ quantity = "%s[%i]/v"%(GrCPop.id, 0)
 ls.add_line_to_display(disp0, "GrC: Vm", quantity, "1mV", "#66c2a5")
 ls.add_column_to_output_file(of0, 'v0', quantity)
 
+quantity = "%s[%i]/tsince"%(mossySpikersPopON.id, 0)
+ls.add_line_to_display(disp1, "mossySpikersPopON 0", quantity, "1ms", "#000000")
+
+quantity = "%s[%i]/tsince"%(mossySpikersPopOFF.id, 0)
+ls.add_line_to_display(disp1, "mossySpikersPopOFF 0", quantity, "1ms", "#FF0000")
+
+
 # Save to LEMS XML file
 lems_file_name = ls.save_to_file()
 
 # Run with jNeuroML
 results1 = pynml.run_lems_with_jneuroml(lems_file_name, nogui=True, load_saved_data=True, plot=True)
 
+'''
 # Run with jNeuroML_NEURON
 results2 = pynml.run_lems_with_jneuroml_neuron(lems_file_name, nogui=True, load_saved_data=True, plot=True)
-
+'''
 
